07cb073273de72c1db12b966b6a74a1b2f388ce4
nhoskins328 <nhoskins328@gmail.com>
2022-12-05 22:52:29 +0000

    new site content

--- refresh.sh
+++ refresh.sh
@@ -1,8 +1,8 @@
 # stop the old
-docker stop index
+docker stop index1
 docker ps -a
-docker rm index
+docker rm index1
 docker ps -a
 docker pull nhoskins/website_repo:latest
 docker images
-docker run -d -p 8080:80 --name index nhoskins/website_repo
+docker run -d --restart unless-stopped -p 8080:80 --name index1 nhoskins/website_repo
