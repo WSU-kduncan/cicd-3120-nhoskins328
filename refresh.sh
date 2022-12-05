08d25086d79d73b9314bb6872774a4b088579b02
nhoskins328 <nhoskins328@gmail.com>
2022-12-05 23:13:02 +0000

    last html mod, hopefully...

--- refresh.sh
+++ refresh.sh
@@ -3,6 +3,8 @@ docker stop index1
 docker ps -a
 docker rm index1
 docker ps -a
+docker image rm -f nhoskins/website_repo
+docker image ls
 docker pull nhoskins/website_repo:latest
 docker images
 docker run -d --restart unless-stopped -p 8080:80 --name index1 nhoskins/website_repo
