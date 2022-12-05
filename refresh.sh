b628688a1cdd94fb83fb68508acc6616ffcea203
nhoskins328 <nhoskins328@gmail.com>
2022-12-05 23:47:55 +0000

    fishing done?

--- refresh.sh
+++ refresh.sh
@@ -3,8 +3,7 @@ docker stop index1
 docker ps -a
 docker rm index1
 docker ps -a
-docker image rm -f nhoskins/website_repo
 docker image ls
 docker pull nhoskins/website_repo:latest
 docker images
-docker run -d --restart unless-stopped -p 8080:80 --name index1 nhoskins/website_repo
+docker run -d --restart always -p 8080:80 --name index1 nhoskins/website_repo
